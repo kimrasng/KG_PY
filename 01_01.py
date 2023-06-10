51a93908d7eac2f0dc5e1e06fc5248a35e579920
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-06-10 10:49:29 +0900

    01_01

---
+++ 01_01.py
@@ -0,0 +1,24 @@
+#콘솔창 (한줄주석)
+
+10
+
+a = 10
+
+a
+a = 10 #a 변수이름 =(대입연산자)    10(변수의 값)
+
+b = 5
+b
+
+result = a + b
+result
+
+aa = "대한"
+bb = "민국"
+
+result2 = aa + bb
+result2
+
+result2 = aa + bb   #   aa(문자), + (연산자), bb(문자)
+
+result = a + b  #   a(숫자), + b(숫자)
