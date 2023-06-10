bf9c8365bf627a21c627f2a7024cb46d9cdd96a8
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-06-10 13:28:58 +0900

    06/10

---
+++ 01_02.py
@@ -0,0 +1,18 @@
+a = 100
+b = 50
+
+a + b
+
+result = a + b
+result
+
+aa = "100"
+bb = "50"
+result2 = aa + bb
+result2
+
+aa = "100"  #   100(숫자 백). "100"(문자 일영영) ===> 숫자형 (숫자를 ""또는''안엠 사용할때)
+aa = '100'
+bb = '50'
+result2 = aa + bb
+result2
---
+++ 01_03.py
@@ -0,0 +1,144 @@
+# a = 100
+# b = 50
+# result = a + b
+# result
+
+# #   100 + 50 = 150
+# print(a, "+", b,"=", result)
+
+
+
+
+
+# a = 100
+# b = 50
+# result = a + b
+# print(a, "+", b, "=", result)
+
+# #100 - 50 = 50
+
+# result = a - b
+# print(a, "-", b, "=",result)
+
+# result = a * b
+# print(a, "*", b, "=", result)
+
+# result = a / b
+# print(a, "/", b, "=", result)
+
+
+
+
+# a = 1000
+# b = 500
+
+# result = a + b
+# print(a, "+", b, "=", result)
+
+# result = a - b
+# print(a, "*", b, "=", result)
+
+# result = a * b
+# print(a, "+", b, "=", result)
+
+# result = a / b
+# print(a, "/", b, "=", result)
+
+#   ''' ''' 여러줄 주석
+
+
+
+
+
+# a = 1000
+# b = 500
+
+# result = a + b
+# print("%d + %d  %d" % (a, b, result))   #   %d(정수형서식) %(구분연산자)
+# print("%d+%d=%d" % (a, b, result))
+# print("%d  +  %d  =  %d" % (a, b, result))  #   print() ===> 출력홤수(1호)
+
+# result = a - b
+# print("%d - %d = %d" % (a, b, result))
+# result = a * b
+# print("%d * %d = %d" % (a, b, result))
+# result = a / b
+# print("%d / %d = %d" % (a, b, result))
+
+
+
+
+
+# a = input()
+# b = input()
+
+# result = a + b
+# print("%d + %d = %d" % (a, b, result))  #   +연결연산자로 문자 + 문자로 실행
+# result = a - b  #   -는 문자 - 문자는 에러
+# print("%d - %d = %d" % (a, b, result))    #   input ==> 입력함수(2호)
+# result = a * b
+# print("%d * %d = %d" % (a, b, result))
+# result = a / b
+# print("%d / %d = %d" % (a, b, result))
+
+
+
+
+# a = int (input())   #   int() = => 정수형 함수 (3호)
+# b = int (input())
+
+# result = a + b
+# print("%d + %d = %d" % (a, b, result))
+
+# result = a - b
+# print("%d - %d = %d" % (a, b, result))
+
+# result = a * b
+# print("%d * %d = %d" % (a, b, result))
+
+# result = a / b
+# print("%d / %d = %d" % (a, b, result))
+
+
+
+# a = int(input("첫번째 숫자를 입력하세요 : "))
+# b = int(input("두번째 숫자를 입력하세요 : "))
+
+# result = a + b
+# print("%d + %d = %d" % (a, b, result))
+
+# result = a - b
+# print("%d - %d = %d" % (a, b, result))
+
+# result = a * b
+# print("%d * %d = %d" % (a, b, result))
+
+# result = a / b
+# print("%d / %d = %d", % (a, b, result))
+
+
+
+
+# a = int(input("첫 번째 숫자를 입력하세요 : "))
+# b = int(input("두 번째 숫자를 입력하세요 : "))
+# c = int(input("세 번째 숫자를 입력하세요 : "))
+# d = int(input("네 번쨰 숫자를 입력하세요"))
+
+# result = a + b + c + d
+# print("%d + %d + %d + %d = %d" % (a, b, c, d, result))
+
+# result = a - b - c - d
+# print("%d + %d + %d + %d = %d" % (a, b, c, d, result))
+
+# result = a * b * c * d
+# print("%d + %d + %d + %d = %d" % (a, b, c, d, result))
+
+# result = a / b / c / d
+# print("%d + %d + %d + %d = %d" % (a, b, c, d, result))
+
+
+name = (input("이름을 입력해주세요 : "))
+age = int(input("나이을 입력해주세요 : "))
+e_mail = (input("이메일를 입력해주세요 : "))
+
+print("이름 : ", name, "나이 : ", age, "이메일 : ", e_mail)
