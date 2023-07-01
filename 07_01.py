e47ba9bb3ad42337fc44469783c43fb69d167753
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-07-01 12:37:20 +0900

    코드가좀 많이이상함

---
+++ 07_01.py
@@ -0,0 +1,41 @@
+# def plus(v1, v2):   # plus() 함수를 정의
+#     result = 0
+#     result = v1 + v2    # 매개변수로 받은 두 값의 합계를 구함
+#     return result   # 반환
+
+# hap = 0
+
+# hap = plus(100, 200)    # 100, 200 두 값 을 전달하면서 plus() 함수를 호출해 hap에 대입
+# print("100과 200의 (plus)함수의 결과는 %d" % hap)   # plus()함수에서 반환된 값 출력
+
+# # 함수는 1. 호출, 2. 실행, 3. 반환, 4. 대입 순으로 실행
+
+# # 함수의 개념과 필요성
+#     # 함수(Function) : '무엇'을 넣으면, '어떤 것'을 돌려주는 요술 상자
+#     # 메서도(Methid)와 차이점 : 함수는 외부에 별도로 존재, 매서드는 클래스 안에 존재
+# # 함수의 형식
+#     # 함수의명()
+
+
+# def calc(v1, v2, op):
+#     result = 0
+#     if op == '+':
+#         result = v1+v2
+#     elif op == '-':
+#         result = v1-v2
+#     elif op == '*':
+#         result = v1*v2
+#     elif op == '/':
+#         result = v1/v2
+
+#     return result
+
+# res = 0
+# var1, var2, oper =0, 0, ""
+
+# oper = input("계산을 입력하세요(+, -, *, /)")
+# var1 = calc(var1, var2, oper)
+
+# print("#계산기 : %d %s %d" % (var1, oper, var2, res))
+
+
